ccfe1a0aa3b847867378ed282ba746fd27d62bc8
BBergsJ <goldlike888@gmail.com>
2021-02-14 23:22:30 +0300

    Техническое задание. Аргегатора Java Вакансий

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
-    public static void main(String[] args) {
-
+    public static boolean result(boolean rs) {
+        return true;
     }
 }
--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,7 +1,14 @@
 import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestTest extends TestCase {
 
+    @org.junit.jupiter.api.Test
+    void result() {
+        assertThat(true, is(true));
+    }
 }
