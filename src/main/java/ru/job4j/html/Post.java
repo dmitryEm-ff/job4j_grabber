20e4d7fb0cb9aaf0d8a6bcbbfac9b23958c5f3a3
BBergsJ <goldlike888@gmail.com>
2021-02-21 19:53:35 +0300

    Модель данных - Post.

---
+++ src/main/java/ru/job4j/html/Post.java
@@ -0,0 +1,31 @@
+package ru.job4j.html;
+
+public class Post {
+    private String name;
+    private String url;
+    private String date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
